9a72ddadd107b39dbaa62982545407a9618075c8
Carter Parrett <cparrett94@hotmail.com>
2020-07-25 19:35:38 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,36 +1,36 @@
-class Genre 
+# class Genre 
   
-  extend Concerns::Findable
+#   extend Concerns::Findable
   
-  attr_accessor :name
-  @@all = []
+#   attr_accessor :name
+#   @@all = []
   
-  def initialize(name)
-    @name = name
-    save
-  end
+#   def initialize(name)
+#     @name = name
+#     save
+#   end
   
-   def self.all
-    @@all
-  end
-  def save
-    @@all << self
-  end
+#   def self.all
+#     @@all
+#   end
+#   def save
+#     @@all << self
+#   end
   
-  def self.destroy_all
-    self.all.clear
-  end
+#   def self.destroy_all
+#     self.all.clear
+#   end
   
-  def self.create(name)
-   genre = Genre.new(name)
-   genre.save
-   genre
-  end
+#   def self.create(name)
+#   genre = Genre.new(name)
+#   genre.save
+#   genre
+#   end
   
-  def self.count
-    @@all.size
-  end
+#   def self.count
+#     @@all.size
+#   end
   
  
   
-end
+# end
