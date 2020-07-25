9f88e21d7558044e4e0a47fc51d0b77b3894508b
Carter Parrett <cparrett94@hotmail.com>
2020-07-25 19:35:49 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,16 +1,16 @@
 
-module Findable
+# module Findable
   
-  def find_by_name(name)
-    self.all.detect{|object| object.name == name}
-  end
+#   def find_by_name(name)
+#     self.all.detect{|object| object.name == name}
+#   end
   
-  def self.find_or_create_by_name(name)
-    if find_by_name(name) == nil
-      name = self.new(name)
-    else
-      find_by_name(name)
-    end
-  end
+#   def self.find_or_create_by_name(name)
+#     if find_by_name(name) == nil
+#       name = self.new(name)
+#     else
+#       find_by_name(name)
+#     end
+#   end
   
-end
+# end
